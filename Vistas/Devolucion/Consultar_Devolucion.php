8275ee3ab430f12242bb9a84aa166f927bbf8efe
Emilio <e.np99business@gmail.com>
2020-05-28 19:00:17 -0500

    Cambiada la estructura de consulta de devolución

--- Vistas/Devolucion/Consultar_Devolucion.php
+++ Vistas/Devolucion/Consultar_Devolucion.php
@@ -27,6 +27,8 @@ else{
   $tupla = $resultado->fetch_assoc();
   $max = $tupla["folio_devolucion"];
 }
+
+$montoDevolucion = 0;
 ?>
 
 <DOCTYPE !html>
@@ -47,6 +49,9 @@ else{
   
 <?php
 if(isset($_POST["buscar"])){
+  $stmtInfoMontoVenta = $enlace->prepare("SELECT SUM(cantidad * valor_unitario) as 'importe' FROM detalle_venta WHERE folio_venta = ?");
+  $stmtInfoMontoVenta->bind_param("i", $folio_venta);
+  
   $stmtInfoDevolucion = $enlace->prepare("SELECT * FROM devolucion WHERE folio_devolucion = ?");
   $stmtInfoDevolucion->bind_param("i", $_POST["folio"]);
   $stmtInfoDevolucion->execute();
@@ -58,13 +63,16 @@ if(isset($_POST["buscar"])){
   $stmtDetalleDevolucion->execute();
   $resultadoDetalle = $stmtDetalleDevolucion->get_result();
   
-  $stmtInfoVenta = $enlace->prepare("SELECT fecha_venta FROM venta WHERE folio_venta = ?");
+  $stmtInfoVenta = $enlace->prepare("SELECT fecha_venta, iva, id_cliente FROM venta WHERE folio_venta = ?");
   $stmtInfoVenta->bind_param("i", $folio_venta);
   
   $stmtInfoEmpleado = $enlace->prepare("SELECT nombre_empleado FROM empleado WHERE rfc_empleado = ?");
   $stmtInfoEmpleado->bind_param("s", $rfc_emp);
   
-  $stmtInfoProducto = $enlace->prepare("SELECT nombre, descripcion FROM producto WHERE clave_producto = ?");
+  $stmtInfoCliente = $enlace->prepare("SELECT nombre FROM cliente WHERE rfc = ?");
+  $stmtInfoCliente->bind_param("s", $id_cliente);
+  
+  $stmtInfoProducto = $enlace->prepare("SELECT nombre, descripcion, precio FROM producto WHERE clave_producto = ?");
   $stmtInfoProducto->bind_param("i", $clav_prod);
 ?>
   <table class="pure-table">
@@ -77,6 +85,8 @@ if(isset($_POST["buscar"])){
         <th>Fecha de la Venta</th>
         <th>RFC Empleado</th>
         <th>Nombre del Empleado</th>
+        <th>RFC Cliente</th>
+        <th>Nombre del Cliente</th>
       </tr>
     </thead>
     <tbody>
@@ -87,20 +97,30 @@ if(isset($_POST["buscar"])){
         $stmtInfoVenta->execute();
   
         $resultadoFecha = $stmtInfoVenta->get_result();
-        $tuplaFechaVenta = $resultadoFecha->fetch_assoc();
+        $tuplaInfoVenta = $resultadoFecha->fetch_assoc();
+  
+        $iva = $tuplaInfoVenta["iva"];
+        $id_cliente = $tuplaInfoVenta["id_cliente"];
   
         $stmtInfoEmpleado->execute();
   
         $resultadoEmpleado = $stmtInfoEmpleado->get_result();
         $tuplaInfoEmpleado = $resultadoEmpleado->fetch_assoc();
+  
+        $stmtInfoCliente->execute();
+  
+        $resultadoCliente = $stmtInfoCliente->get_result();
+        $tuplaInfoCliente = $resultadoCliente->fetch_assoc();
       ?>
       <tr>
         <td><?=$_POST["folio"]?></td>
         <td><?=$tuplaInfoDevolucion["fecha"]?></td>
         <td><?=$tuplaInfoDevolucion["folio_venta"]?></td>
-        <td><?=$tuplaFechaVenta["fecha_venta"]?></td>
+        <td><?=$tuplaInfoVenta["fecha_venta"]?></td>
         <td><?=$tuplaInfoDevolucion["rfc_empleado"]?></td>
         <td><?=$tuplaInfoEmpleado["nombre_empleado"]?></td>
+        <td><?=$id_cliente?></td>
+        <td><?=$tuplaInfoCliente["nombre"]?></td>
       </tr>
     </tbody>
   </table>
@@ -113,6 +133,8 @@ if(isset($_POST["buscar"])){
         <th>Nombre del Producto</th>
         <th>Descripción del Producto</th>
         <th>Cantidad</th>
+        <th>Precio</th>
+        <th>Importe</th>
         <th>Motivo</th>
       </tr>
     </thead>
@@ -122,15 +144,37 @@ if(isset($_POST["buscar"])){
       $stmtInfoProducto->execute();
       $resultadoInfoProducto = $stmtInfoProducto->get_result();
       $tuplaInfoProducto = $resultadoInfoProducto->fetch_assoc();
+        
+      $montoDevolucion += $tuplaInfoDetalle["cantidad"] * $tuplaInfoProducto["precio"];
       ?>
       <tr>
         <td><?=$clav_prod?></td>
         <td><?=$tuplaInfoProducto["nombre"]?></td>
         <td><?=$tuplaInfoProducto["descripcion"]?></td>
         <td><?=$tuplaInfoDetalle["cantidad"]?></td>
+        <td><?=$tuplaInfoProducto["precio"]?></td>
+        <td><?=$tuplaInfoDetalle["cantidad"] * $tuplaInfoProducto["precio"]?></td>
         <td><?=$tuplaInfoDetalle["motivo"]?></td>
       </tr>
       <?php } ?>
+    </tbody>
+  </table>
+  
+  <table class="pure-table">
+    <thead>
+      <br><legend>Tabla de Información de Pago de la Devolución</legend><br>
+      <tr>
+        <th>Subtotal</th>
+        <th>IVA</th>
+        <th>Total</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?=$montoDevolucion?></td>
+        <td><?=$montoDevolucion * $iva?></td>
+        <td><?=($montoDevolucion + ($montoDevolucion * $iva))?></td>
+      </tr>
     </tbody>
   </table>
 <?php
